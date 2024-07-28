13c9df9d0c90e91dfb0201d5645babe2fbba42f5
jimyag <git@jimyag.com>
2024-07-29 00:03:56 +0800

    order by derper version

--- derper/build.sh
+++ derper/build.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
-
+set -x
 # Fetching the release tags from the GitHub API
-release_tags=$(curl -s https://api.github.com/repos/tailscale/tailscale/releases | grep '"tag_name":' | sed -n '1,3s/.*"v\([^"]*\)".*/\1/p')
-
+release_tags=$(curl -s https://api.github.com/repos/tailscale/tailscale/releases |\
+  grep '"tag_name":' | \
+  sed -n '1,3s/.*"v\([^"]*\)".*/\1/p' | \
+  sort -V)
+echo $release_tags
 docker buildx build --platform linux/amd64 \
     --file ./derper/Dockerfile --push \
     --tag ghcr.io/jimyag/derper:latest \
