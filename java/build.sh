89315e48d037d0fc8ee3d8604763356d4286b258
jimyag <jiaminyag@gmail.com>
2024-07-29 23:56:15 +0800

    set base_url

--- java/build.sh
+++ java/build.sh
@@ -3,7 +3,7 @@ set -ex
 jdk_versions=("1.8.0_411" "1.8.0_111")
 jre_versions=("1.8.0_411" "1.8.0_111")
 
-base_url="$STATIC_BASE_URL"
+base_url="https://r2.jimyag.com/"
 
 for jdk_version in "${jdk_versions[@]}"; do
     dst="jdk-${jdk_version}-linux-x64.tar.gz"
