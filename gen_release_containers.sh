9b658e6428cf5cda20b0556b6dbd44ec1877d990
jimyag <git@jimyag.com>
2025-05-11 12:00:04 +0800

    更新 gen_release_containers.sh 脚本，优化获取最新标签的方式，并修改 git diff 命令以仅显示新增和修改的文件。

--- gen_release_containers.sh
+++ gen_release_containers.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 set -x
-latest_tag=$(git describe --tags --abbrev=0 --always )
+
+latest_tag=$(git describe --tags --abbrev=0 --always)
 previous_tag=$(git describe --tags --abbrev=0 "$latest_tag"^ 2>/dev/null)
 
 if [[ -z $previous_tag ]]; then
     previous_tag=$(git rev-list --max-parents=0 HEAD)
 fi
 
-git diff --name-only "$previous_tag" "$latest_tag" |\
+git diff --name-status --diff-filter=AM "$previous_tag" "$latest_tag" |\
+    awk '{print $2}' |\
     grep -v -e ".git" -e "README" -e "gen_release_containers.sh" -e "gen_tag.sh" -e "java" |\
     while IFS= read -r line; do
         dirname=$(dirname "$line")
