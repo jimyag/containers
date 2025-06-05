f04d65f68223c070fc12c030029297c3e92eae47
jimyag <git@jimyag.com>
2025-06-06 00:29:52 +0800

    更新 openconnect/build.sh 脚本，将 Docker 构建命令中的 --load 参数更改为 --push，以支持将镜像推送到远程仓库。

--- openconnect/build.sh
+++ openconnect/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 docker buildx build --platform linux/amd64 \
-    --file ./openconnect/Dockerfile --load \
+    --file ./openconnect/Dockerfile --push \
     --tag ghcr.io/jimyag/openconnect:latest \
     ./openconnect
